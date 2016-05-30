68ae45b5a5dd90869bb9075a705ec5ccbeb180df
Gabriel Monteiro <gpontesmonteiro@gmail.com>
2016-05-30 12:55:19 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+	
+	<meta http-equiv="Content-Type" content="text/html, charset=utf-8">
+	<title>Controle SEFAC</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" href="favicon.ico" >
+	</head>
+	<body>
+		<div class= "body">
+			<div class = "consoleindex">
+			<h1>CONTROLE SEFAC</h1>
+				<div class = "menu">
+					<a href="cadastrar.php"><b>CADASTRAR PROPRIEDADE</b></a>
+					<a href="deletar.php"><b>DELETAR PROPRIEDADE</b></a>
+					<a href="buscar.php"><b>BUSCAR</b></a>
+				</div>
+			</div>
+		</div>
+		<?php include'rodape.php';?>
+	</body>
+</html>

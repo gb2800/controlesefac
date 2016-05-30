ad966fa66aa9def48027a218f1ae664efef2c1f9
Gabriel Monteiro <gpontesmonteiro@gmail.com>
2016-05-30 12:56:05 -0300

    Create buscar.php

---
+++ buscar.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+	
+	<meta http-equiv="Content-Type" content="text/html, charset=utf-8">
+	<title>Controle SEFAC</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" href="favicon.ico" >
+	
+</head>
+<body>  
+	<div class= "body">
+		<div class = "consoleindex">
+			<div class="bmenu">
+				<form name="searchform" method="POST" action="resultado.php">
+				<b>BUSCAR PROPRIEDADE:&nbsp;</b><input type="text" name="buscar">	
+				<input type="submit" value="Buscar"/>
+
+				</form > 
+			</div>	
+			<div class ="buscam">
+				<a href="cadastrar.php"><b>CADASTRAR PROPRIEDADE</b></a>
+				<a href="deletar.php"><b>DELETAR PROPRIEDADE</b></a>
+			</div>
+		</div>
+	</div>
+			
+<?php include'rodape.php';?>
+</body>                                                                 
+
+</html>                
